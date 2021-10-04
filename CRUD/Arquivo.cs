1d600f019286aca02731f10bd326a571bc12befd
Iagor M.L <iagorluchesi@gmail.com>
2021-10-03 22:46:03 -0400

    final

--- CRUD/Arquivo.cs
+++ CRUD/Arquivo.cs
@@ -5,50 +5,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CRUD
-{
-   public class Arquivo {
-       public static string Ler() {
-            Console.WriteLine("----------------------------------");
-            string Path = @"C:\Users\iagor.luchesi\Documents\Dev\teste.txt";
-            StreamReader t = new StreamReader(Path);
-            t = File.OpenText(Path);
-            string lines = t.ReadToEnd();
-            t.Close();
-            return lines;
+namespace CRUD {
+    public class Arquivo {
+        public static string Ler() {
+            
+                string Path = @"C:\Users\iagor\Documents\texto.txt";
+                StreamReader t = new(Path);
+                t = File.OpenText(Path);
+                string lines = t.ReadToEnd();
+                t.Close();
+                return lines;
+            }
+
+        public static void Apagar() {
+            try {
+                string caminho = @"C:\Users\iagor\Documents\texto.txt";
+                File.Delete(caminho);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally {
+                Console.WriteLine("FIM.");
+            };
 
         }
 
-        public static void Apagar()
-        {
-            string caminnho = @"C:\Users\iagor.luchesi\Documents\Dev\teste.txt";
-            File.Delete(caminnho);
+        public static void Criar() {
+            try {
+                string caminho = @"C:\Users\iagor\Documents\texto.txt";
+                File.Create(caminho);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally {
+                Console.WriteLine("FIM.");
+            };
         }
 
-    /*    public static string Escrever()
-        {
-            try
-            {
-                //Pass the filepath and filename to the StreamWriter Constructor
-                StreamWriter sw = new StreamWriter(@"C:\Users\iagor.luchesi\Documents\Dev\texto.txt");
-                //Write a line of text
-                sw.WriteLine("Hello World!!");
-                //Write a second line of text
-                sw.WriteLine("cu");
-                //Close the file
-                sw.Close();
-                return null;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Burro errou Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Nice acabou aqui.");
-            }
-
-        }*/
 
     }
 }
--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -10,21 +10,16 @@ namespace CRUD
             while (true)
             {
                 Console.WriteLine("Escolha uma ação com um número [1,2,3,4,5]: ");
-                Console.WriteLine("1 - LER  2 - ESCREVER  3 - ATUALIZAR  4 - EXCLUIR  5 - SAIR"); 
+                Console.WriteLine("1 - LER  2 - ESCREVER  3 - CRIAR  4 - EXCLUIR  5 - SAIR"); 
                     
                 int escolha = int.Parse(Console.ReadLine());
                 if (escolha == 1) Console.WriteLine(Arquivo.Ler());
                 if (escolha == 2) try
                     {
-                        //Pass the filepath and filename to the StreamWriter Constructor
-                        StreamWriter sw = new StreamWriter(@"C:\Users\iagor.luchesi\Documents\Dev\teste.txt");
-                        //Write a line of text
+                        StreamWriter sw = new(@"C:\Users\iagor\Documents\texto.txt");
                         sw.WriteLine("Hello World!!");
-                        //Write a second line of text
-                        sw.WriteLine("From the StreamWriter class");
-                        //Close the file
+                        sw.WriteLine("salve");
                         sw.Close();
-                        // refazer ta mt ruim
                     }
                     catch (Exception e)
                     {
@@ -34,19 +29,20 @@ namespace CRUD
                     {
                         Console.WriteLine("Fim da atividade.");
                     };
+                if (escolha == 3) try { Arquivo.Criar(); }
 
-                if (escolha == 4) try
-                {
-                        Arquivo.Apagar();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Exception: " + e.Message);
-                }
-                finally
-                {
-                    Console.WriteLine("FIM.");
-                }; 
+                    //Console.WriteLine("digite o caminho para criar o arquivo : ");
+                    //string path = Console.ReadLine();
+                    //File.Create(path);
+                    //erro de acess denied :/ 
+
+                    catch (Exception e) {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                    finally {
+                        Console.WriteLine("FIM.");
+                    };
+                if (escolha == 4) Arquivo.Apagar();
 
                 if (escolha == 5) break;
 
